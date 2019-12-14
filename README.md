45dc534ca41fd79040e12e23ed5c5cbf301fa4d3
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2019-12-14 12:51:53 +0100

    Initial commit

--- README.md
+++ README.md
@@ -5,3 +5,29 @@
 [![Build Status](https://travis-ci.com/jagot/PDFToc.jl.svg?branch=master)](https://travis-ci.com/jagot/PDFToc.jl)
 [![Build Status](https://ci.appveyor.com/api/projects/status/github/jagot/PDFToc.jl?svg=true)](https://ci.appveyor.com/project/jagot/PDFToc-jl)
 [![Codecov](https://codecov.io/gh/jagot/PDFToc.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/jagot/PDFToc.jl)
+
+Small helper library to generate PDF table of contents according to http://blog.tremily.us/posts/PDF_bookmarks_with_Ghostscript/
+
+Usage:
+
+```julia
+using PDFToc
+
+addtoc("document.pdf", "document.toc",
+       r"regex pattern 1" => m -> ("title", page, level),
+       r"regex pattern 2" => m -> ("title", page, level),
+       ...;
+       title="Document title",
+       author="Document author",
+       offset=4)
+```
+
+The result will be saved to `document-out.pdf`. The file
+`document.toc` is a plain text file, where each line represents one
+entry in the ToC. The regex patterns will be tested on each line; the
+first one that matches will have its corresponding lambda function
+applied to the regex match. The return value is assumed to return a
+`Tuple{String,Int,Int}` containing the title, page, and level,
+respectively of the ToC entry. One can also add metadata such as
+document title and author(s), as well as custom ones
+(e.g. year). Finally, `offset` will be added to each page number.
--- src/PDFToc.jl
+++ src/PDFToc.jl
@@ -1,5 +1,95 @@
 module PDFToc
+struct Heading
+    title::String
+    page::Int
+    children::Vector{Heading}
+end
+Heading(title, page) = Heading(title, page, Vector{Heading}())
 
-greet() = print("Hello World!")
+function Base.show(io::IO, ::MIME"text/plain", h::Heading)
+    write(io, "$(h.title) ... $(h.page)")
+    buf = IOBuffer()
+    for c in h.children
+        write(buf, "\n")
+        show(buf, MIME"text/plain"(), c)
+    end
+    for l in split(String(take!(buf)), "\n")
+        write(io, "    $(l)\n")
+    end
+end
+
+function Base.show(io::IO, ::MIME"text/plain", hs::Vector{Heading})
+    foreach(h -> show(io, MIME"text/plain"(), h), hs)
+end
+
+
+function readtoc(filename, patterns...)
+    headings = open(filename, "r") do file
+        map(eachline(file)) do line
+            for (p,f) in patterns
+                m = match(p, line)
+                isnothing(m) && continue
+                return f(m)
+            end
+            println(line)
+        end
+    end
+    s = [Vector{Heading}()]
+    for h in headings
+        title,page,level = h[1], h[2], h[3]
+        level > length(s) && push!(s, s[end][end].children)
+        while level < length(s)
+            pop!(s)
+        end
+        push!(s[end], Heading(title, page))
+    end
+
+    first(s)
+end
+
+function psescape(s)
+    if isascii(s)
+        "("*replace(replace(s, "(" => "\\("), ")" => "\\)")*")"
+    else
+        cus = vcat(0xfeff, transcode(UInt16, s))
+        "<"*join(map(c -> string(c, base=16, pad=4), cus), " ")*">"
+    end
+end
+
+function pdfmarks(io::IO, h::Heading; offset=0)
+    write(io, "[")
+    c = length(h.children)
+    c > 0 && write(io, "/Count $(c) ")
+    write(io, "/Title $(psescape(h.title)) /Page $(h.page+offset)")
+    write(io, " /OUT pdfmark\n")
+    foreach(hc -> pdfmarks(io, hc; offset=offset), h.children)
+end
+
+function pdfmarks(io::IO, hs::Vector{Heading};
+                  title=nothing, author=nothing, year=nothing,
+                  kwargs...)
+    foreach(h -> pdfmarks(io, h; kwargs...), hs)
+    metadata = String[]
+    !isnothing(title) && push!(metadata, "/Title $(psescape(title))")
+    !isnothing(author) && push!(metadata, "/Author $(psescape(author))")
+    !isnothing(year) && push!(metadata, "/Year ($(year))")
+    if !isempty(metadata)
+        push!(metadata, "/DOCINFO pdfmark")
+        write(io, "[ "*join(metadata, "\n  "))
+    end
+end
+
+pdfmarks(h; kwargs...) = pdfmarks(stdout, h; kwargs...)
+
+function addtoc(pdffile, tocfile, patterns...; kwargs...)
+    toc = readtoc(tocfile, patterns...)
+    filename = first(splitext(pdffile))
+    open("$(filename).pdfmarks", "w") do file
+        pdfmarks(file, toc; kwargs...)
+    end
+    run(`gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=$(filename)-out.pdf $(pdffile) $(filename).pdfmarks`)
+end
+
+export readtoc, pdfmarks, addtoc
 
 end # module
